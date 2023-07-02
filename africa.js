2e239ff198552da741caa30349e772ec3a4a2c19
kinnyode01~ <daakinyode@gmail.com>
2023-07-03 00:14:28 +0100

    yes

---
+++ africa.js
@@ -0,0 +1,23 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('dykassets-animation');
+        entry.target.classList.add('flexbox-animation');
+        
+      }
+    });
+  });
+  
+  const viewbox = document.querySelectorAll('.dykassets');
+  viewbox.forEach(image => {
+    observer.observe(image);
+  
+    const viewbox = document.querySelectorAll('.flexbox');
+  viewbox.forEach(image => {
+    observer.observe(image);
+
+});
+  
+});
+  
