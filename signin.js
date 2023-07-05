ecca52e717641fb197b94f094efd9798fb96c6e1
kinnyode01~ <daakinyode@gmail.com>
2023-07-05 17:03:41 +0100

    testingggg

--- signin.js
+++ signin.js
@@ -3,14 +3,14 @@ var attempt = 3; // Variable to count number of attempts.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "Formget" && password == "formget#123"){
-alert ("Login successfully");
-window.location = "europe.html"; // Redirecting to other page.
+if ( username == "Login" && password == "mycountry"){
+alert ("Login successful. You are now being redirected based on your location.");
+window.location = "africa.html"; // Redirecting to other page.
 return false;
 }
 else{
 attempt --;// Decrementing by one.
-alert("You have left "+attempt+" attempt;");
+alert("You have "+attempt+" attempt(s) left.");
 // Disabling fields after 3 attempts.
 if( attempt == 0){
 document.getElementById("username").disabled = true;
