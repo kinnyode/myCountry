fc533a46f47019bbd950e14a153b1f205cb866f6
kinnyode <daakinyode@gmail.com>
2023-07-06 16:14:08 +0100

    yeah

--- getstarted.js
+++ getstarted.js
@@ -3,30 +3,46 @@ function validateform(){
     var name=document.myform.name.value;
     var email= document.myform.email.value;  
     var password=document.myform.password.value;
-   
+   var password2=document.myform.password2.value;
      
      if (name==""){  
         document.getElementById("results").innerHTML = ("Please enter your full name & username.")  ;
       return false;  
        }
   
-    
+       else if (email==""){
+        document.getElementById("results").innerHTML = ("Please enter a valid email.")  ;
+      return false;
+  }
 
-          else if (password==""){
+
+
+
+
+          else if (password ==""){
             document.getElementById("results").innerHTML = ("Please enter a password.")  ;
           return false;
       }
 
-      
-
-    else if(password.length<6){  
+       if(password.length<6){  
         document.getElementById("results").innerHTML = ("Password must be greater than six characters.")  ; 
       return false;  
       }  
 
 
+      
+       if (password2==""){
+        document.getElementById("results").innerHTML = (" Confirm your password.")  ; 
+        return false; 
+      }
+
+      else if (password != password2){
+        document.getElementById("results").innerHTML = ("Passwords do not match ejoor.")  ; 
+        return false; 
+      }
+
       else if(password.length>= 6){
-        alert ("Sign Up successful. You are now being redirected based on your location.");
+        alert ("Sign Up to myCountry successful. You are now being redirected based on your location.");
         window.location = "africa.html"; // Redirecting to other page.
         return false;
       }
