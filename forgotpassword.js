14518482fca7baaa51da1abef5c7b161a97f914f
kinnyode <daakinyode@gmail.com>
2023-07-07 14:50:30 +0100

    yeah

---
+++ forgotpassword.js
@@ -0,0 +1,40 @@
+function validate(){  
+    var password1=document.myform.password1.value;
+   var password2=document.myform.password2.value;
+     
+     if (password1==""){  
+        document.getElementById("results").innerHTML = ("Please enter a new password.")  ;
+      return false;  
+       }
+  
+       
+    else  if (password1.length<6 ){
+        document.getElementById("results").innerHTML = ("Passwords must be greater than six characters.")  ;
+      return false;
+  }
+
+     
+  if (password2==""){  
+    document.getElementById("results").innerHTML = ("Please confirm your password.")  ;
+  return false;  
+   }
+
+          if (password2.length<6){
+            document.getElementById("results").innerHTML = ("Passwords must be greater than six characters.") ;
+          return false;
+      }
+
+      if (password1 != password2){
+        document.getElementById("results").innerHTML = ("Passwords do not match ejoor.")  ; 
+        return false; 
+      }
+
+      else if(password1 ==password2){
+        alert ("Password change successful. You are now being redirected to the Login page.");
+        window.location = "signin.html"; // Redirecting to other page.
+        return false;
+      }
+
+     
+    }  
+
