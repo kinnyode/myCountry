3acad17194a078ca23b8f795c393349c58b51cfa
kinnyode <daakinyode@gmail.com>
2023-07-08 02:25:21 +0100

    dada

--- getstarted.js
+++ getstarted.js
@@ -1,17 +1,36 @@
 function validateform(){  
 
     var name=document.myform.name.value;
+    var username= document.myform.username.value;  
     var email= document.myform.email.value;  
     var password=document.myform.password.value;
    var password2=document.myform.password2.value;
      
      if (name==""){  
-        document.getElementById("results").innerHTML = ("Please enter your full name & username.")  ;
+        document.getElementById("results").innerHTML = ("Please enter your full name.")  ;
       return false;  
        }
+
+       if (username==""){  
+        document.getElementById("results").innerHTML = ("Please enter your username.")  ;
+      return false;  
+       }
+
+       else if (email==""){
+        document.getElementById("results").innerHTML = ("Please enter an email.")  ;
+      return false;
+  }
+
+
+       var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+            if (reg.test(email) == false) 
+            {
+              document.getElementById("results").innerHTML = ("Invalid email.")  ;
+                return (false);
+            }
   
        else if (email==""){
-        document.getElementById("results").innerHTML = ("Please enter a valid email.")  ;
+        document.getElementById("results").innerHTML = ("Please enter an email.")  ;
       return false;
   }
 
@@ -37,9 +56,11 @@ function validateform(){
       }
 
       else if (password != password2){
-        document.getElementById("results").innerHTML = ("Passwords do not match ejoor.")  ; 
+        document.getElementById("results").innerHTML = ("Passwords do not match.")  ; 
         return false; 
       }
+
+      
 
       else if(password.length>= 6){
         alert ("Sign Up to myCountry successful. You are now being redirected based on your location.");
@@ -47,5 +68,7 @@ function validateform(){
         return false;
       }
 
+      
+
      
-  
+    }  
